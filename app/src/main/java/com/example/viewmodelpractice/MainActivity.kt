86b26dc76e1411a7a90cde62d24ca09ea91b1c7b
limonreza00 <limonreza3.1416@gmail.com>
2025-03-02 12:17:06 +0600

    feat: Implement ViewModel integration and UI update
    This commit integrates `MyViewModel` with the Compose UI and enables UI updates based on the ViewModel's state.
    - Added `androidx.lifecycle:lifecycle-viewmodel-compose` as a dependency.
    - Created `MyViewModel` to manage the count state and increment logic.
    - Modified the `App` composable to use `viewModel()` to get an instance of `MyViewModel`.
    - Replaced the local count state in `App` with the `count` state from `MyViewModel`.
    - Updated the increment button to call `viewModel.increment()` to update the count.
    - Added `MyViewModel` class with `_count` as a `mutableIntStateOf` to keep track of the incremented number and a function called `increment` to increase the count.

--- app/src/main/java/com/example/viewmodelpractice/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelpractice/MainActivity.kt
@@ -7,17 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.viewmodelpractice.ui.theme.ViewModelPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,16 +29,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun App(){
-
-    val count = remember { mutableIntStateOf(0) }
+  val viewModel : MyViewModel = viewModel()
     Column (
         modifier = Modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
-        Text("Count is ${count.intValue}")
-        Button(onClick = { count.intValue++ }) {
+        Text("Count is ${viewModel.count.intValue}")
+        Button(onClick = { viewModel.increment() }) {
             Text("Increment")
         }
 
--- app/src/main/java/com/example/viewmodelpractice/MyViewModel.kt
+++ app/src/main/java/com/example/viewmodelpractice/MyViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.viewmodelpractice
 
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.ViewModel
 
 class MyViewModel :ViewModel() {
 
+    private val _count = mutableIntStateOf(0)
+    val count = _count
+
+    fun increment (){
+        _count.intValue++
+    }
 
 }
