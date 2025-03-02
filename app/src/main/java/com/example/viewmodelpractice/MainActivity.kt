71c6f3967bda6aac62db5b62fa5c2147aaa45682
limonreza00 <limonreza3.1416@gmail.com>
2025-03-02 11:57:35 +0600

    feat: Implement MyViewModel and basic UI
    This commit introduces the `MyViewModel` class and sets up a basic user interface in `MainActivity`.
    - Created `MyViewModel` which extends `ViewModel`.
    - Added a `Composable` function `App` in `MainActivity` that displays a counter and an increment button.
    - Implemented a counter using `mutableIntStateOf` and `remember`.
    - added `.idea` directory containing `vcs.xml`, `appInsightsSettings.xml` and `deploymentTargetSelector.xml`

--- app/src/main/java/com/example/viewmodelpractice/MainActivity.kt
+++ app/src/main/java/com/example/viewmodelpractice/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.viewmodelpractice.ui.theme.ViewModelPracticeTheme
@@ -19,10 +26,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ViewModelPracticeTheme {
-
-
+                App()
             }
         }
     }
 }
 
+@Composable
+fun App(){
+
+    val count = remember { mutableIntStateOf(0) }
+    Column (
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text("Count is ${count.intValue}")
+        Button(onClick = { count.intValue++ }) {
+            Text("Increment")
+        }
+
+    }
+}
